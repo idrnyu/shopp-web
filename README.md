8be2977bc3981f156196f5e5c273db38a5bcb534
idrnyu <27189085+idrnyu@users.noreply.github.com>
2023-04-12 15:38:14 +0000

    Deploying to www from @ idrnyu/shopp-web@71948f4fdb3389f33e5687546befc4a90fc377ac 🚀

--- README.md
+++
@@ -1,4 +0,0 @@
-# shopp-web
-矿机前端页面-vue
-
-发布izhi
---
+++ assets/detail-d2cf699b.js
@@ -0,0 +1,1 @@
+import{j as t,o,c as a,v as n,u as s,z as r}from"./index-d5c9b73e.js";const _=t({__name:"detail",setup(u){const e=r();return(c,l)=>(o(),a("div",null," 详情页面 "+n(s(e).query.q),1))}});export{_ as default};
