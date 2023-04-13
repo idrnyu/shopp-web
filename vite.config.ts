32298e753d2d6c1a954d51f012f45588ff760355
idrnyu <834999326@qq.com>
2023-04-13 10:52:25 +0800

    修改vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 8000,
-  },
-  resolve: {
-    // 导入别名
-    alias: {
-      "@": resolve(__dirname, "src")
-    }
-  },
-  base: process.env.NODE_ENV === 'production' ? 'shopp-web' : '/',
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const env=loadEnv(mode, process.cwd());
+  console.log(env.VITE_APP_ENV);
+  return {
+    plugins: [vue()],
+    server: {
+      port: 8000,
+    },
+    resolve: {
+      // 导入别名
+      alias: {
+        "@": resolve(__dirname, "src")
+      }
+    },
+    base: env.VITE_APP_ENV === 'github' ? 'shopp-web' : '/',
+  }
 });
