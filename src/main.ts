3d9f54f6ecbcab10ab320607d502ba09729b1fc7
idrnyu <27189085+idrnyu@users.noreply.github.com>
2023-06-15 00:48:19 +0000

    添加vconsole组件

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,10 @@ import router from './router/index';
 import App from "./App.vue";
 import "./index.css";
 
+import VConsole from 'vconsole';
+
+const vConsole = new VConsole();
+// 或者使用配置参数来初始化，详情见文档
+const vConsole = new VConsole({ theme: 'dark' });
+
 createApp(App).use(router).mount("#shopp");
