fe1105aec46dc08fb4a0095ffe9ba7aa173fc024
idrnyu <27189085+idrnyu@users.noreply.github.com>
2023-06-15 01:03:54 +0000

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import "./index.css";
 
 import VConsole from 'vconsole';
 
-const vConsole = new VConsole();
-// 或者使用配置参数来初始化，详情见文档
 const vConsole = new VConsole({ theme: 'dark' });
 
 createApp(App).use(router).mount("#shopp");
