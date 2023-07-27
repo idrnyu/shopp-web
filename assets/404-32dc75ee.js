85e406612700d01672a3f037c3043e9bfcc289b0
idrnyu <27189085+idrnyu@users.noreply.github.com>
2023-07-27 11:49:57 +0000

    Deploying to www from @ idrnyu/shopp-web@722d277b8d01235c9c98f345d6e1b62736a4d329 🚀

---
+++ assets/404-32dc75ee.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-d0cc2c15.js";const o={};function r(t,_){return c(),n("div",null," 404 ")}const a=e(o,[["render",r]]);export{a as default};
---
+++ assets/detail-ab97b6e7.js
@@ -0,0 +1,1 @@
+import{j as t,o,c as a,v as n,u as s,z as r}from"./index-d0cc2c15.js";const _=t({__name:"detail",setup(u){const e=r();return(c,l)=>(o(),a("div",null," 详情页面 "+n(s(e).query.q),1))}});export{_ as default};
